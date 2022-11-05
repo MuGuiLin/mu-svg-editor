12b00999e9d357d2d7f73e0ace0091415dce8a98
MuGuiLin <muguilin@foxmail.com>
2022-11-05 10:20:54 +0800

    update

--- src/components/MuSvgEditor/tool/index.tsx
+++ src/components/MuSvgEditor/tool/index.tsx
@@ -12,6 +12,8 @@ export default defineComponent({
         return (
             <aside class={style.tool}>
                 <h3>绘图</h3>
+				<p>直线</p>
+				<p>圆弧</p>
                 <h3>控件</h3>
             </aside>
         );
